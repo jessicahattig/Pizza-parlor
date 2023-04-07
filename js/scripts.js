ec87a8cd4a18b4ed22659f14f5d8db3f9bfa589d
jessicahattig <jessicahattig@gmail.com>
2023-04-07 16:12:31 -0700

    Added styling.

--- js/scripts.js
+++ js/scripts.js
@@ -9,11 +9,6 @@ PizzaOrder.prototype.addOrder = function(order) {
     this.pizzaTotal[order.id] = order;
 };
 
-PizzaOrder.prototype.assignId = function() {
-    this.currentId += 1;
-    return this.currentOrder;
-};
-
 PizzaOrder.prototype.findOrder = function(id) {
     if (this.pizzaTotal[id] !== undefined) {
 return this.pizzaTotal[id];
@@ -30,32 +25,19 @@ function Cost (pizzaSize, toppingOne, toppingTwo, toppingThree) {
 };
 
 Cost.prototype.totalCost = function() {
-    return sum([pizzaSize.value] + [toppingOne.value] + [oppingTwo.value] + [toppingThree.value]);
+    return sum([pizzaSize.value] + [toppingOne.value] + [oppingTwo.value] + [toppingThree.value])
 };
 
 // User Interface Logic
-const priceSmall = 10.00;
-const priceMedium = 12.00;
-const priceLarge = 17.00;
-const priceToppingOne = 0.50; 
-const priceToppingTwo = 2.00;
-const priceToppingThree = 3.00;
-const priceToppingFour = 4.00;
-let total = 0;
-
 function handleFormSubmission(event) {
     event.preventDefault();
-    const size = document.getElementById('size').value;
+    const pizzaSize = document.getElementById('size').value;
     const toppingOne = document.getElementById('toppingOne').value;
     const toppingTwo = ddocument.getElementById('toppingTwo').value;
     const toppingThree = document.getElementById('toppingThree').value;
-    let order = new order(size, toppingOne, toppingTwo, toppingThree);
+    let order = new order(pizzaSize, toppingOne, toppingTwo, toppingThree);
     pizzaOrder.addOrder(new PizzaOrder)
 };
-
-function addOrder
-    
-
 
 window.addEventListener("submit", function()){
     document.querySelector("div#menu-group").addEventListener("submit", displayCost);
