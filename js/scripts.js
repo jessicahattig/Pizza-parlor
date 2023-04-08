4b49e23d47562268a58799a7e8847aa41cd79c7f
jessicahattig <jessicahattig@gmail.com>
2023-04-07 17:02:31 -0700

    WIP: js event listeners.

--- js/scripts.js
+++ js/scripts.js
@@ -31,15 +31,16 @@ Cost.prototype.totalCost = function() {
 // User Interface Logic
 function handleFormSubmission(event) {
     event.preventDefault();
-    const pizzaSize = document.getElementsByName('pizzaSize').value;
-    const toppingOne = document.getElementByName('toppingOne').value;
-    const toppingTwo = ddocument.getElementByName('toppingTwo').value;
-    const toppingThree = document.getElementByName('toppingThree').value;
-    let order = new order(pizzaSize, toppingOne, toppingTwo, toppingThree);
-    pizzaOrder.addOrder(new PizzaOrder)
+    const smallPizza = document.getElementsById('small').value;
+    const mediumPizza = document.getElementsById('medium').value;
+    const largePizza = document.getElementsById('large').value;
+    const cheeseTopping = document.getElementsById('cheese').value;
+    const pepperoniTopping = document.getElementsById('pepperoni').value;
+    const pineappleTopping= document.getElementsById('pineapple').value;
+    const spinachTopping = document.getElementsById('spinach').value;
+    let order = new order(smallPizza, mediumPizza, largePizza, cheeseTopping, pepperoniTopping pineappleTopping, spinachTopping) 
 };
-
-function displayCost(event) {
+function addOrder(event) {
     
 }
 
@@ -47,8 +48,8 @@ function submitOrder(event){
 
 }
 
-
 window.addEventListener("submit", function()){
+    document.querySelector("div#addOrder").addEventListener("submit", function handleFormSubmission);
     document.querySelector("div#addOrder").addEventListener("submit", function handleFormSubmission);
     document.querySelector(id="btn").addEventListener("submit", handleDelete);
 };
