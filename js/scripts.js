061e6bccdb887fba8ed0cbfd2f9bfa643172b615
jessicahattig <jessicahattig@gmail.com>
2023-04-13 10:23:48 -0700

    WIP

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,8 @@
 //Buisness Logic for Pizza -------------
-function PizzaOrder() {
-this.pizzaTotal = {};
-this.currentOrder = 0;
+function PizzaOrder(toppings, size) {
+this.toppings = toppings;
+this.size = size;
+this.currentOrder = 0; 
 };
 
 PizzaOrder.prototype.addOrder = function(order) {
@@ -25,49 +26,55 @@ addOrder.prototype.totalCost = function() {
     return this.pizzaSize + " " + pizzaToppings
 }};
 
+//global variables
+// const small = 10.00;
+// const medium = 12.00;
+// const large = 15.00;
+// const cheese = 0.00; 
+// const pepperoni = 2.00;
+// const pineapple = 3.00;
+// const spinach = 4.00;
+// let total = 0;
+
 // User Interface Logic
-const small = 10.00;
-const medium = 12.00;
-const large = 15.00;
-const cheese = 0.00; 
-const pepperoni = 2.00;
-const pineapple = 3.00;
-const spinach = 4.00;
-let total = 0;
-
-function calculateSize(element) {
-    if (element == null) {
-        return;
-}
-    switch(element.value) {
-        case "10":
-            subtotal = small;
-            break;
-        case "12":
-            subtotal = medium;
-            break;
-        case "15":
-            subtotal = large;
-            break;
-}
-addOrder();
+// function calculateSize(element) {
+//     if (element == null) {
+//         return;
+// }
+//     switch(element.value) {
+//         case "10":
+//             subtotal = small;
+//             break;
+//         case "12":
+//             subtotal = medium;
+//             break;
+//         case "15":
+//             subtotal = large;
+//             break;
+// }
+// addOrder();
 
 
 
 
-const pizzaSize = document.querySelector(".pizzaSize")
-const pizzaToppings = document.querySelector(".pizzaToppings")
-const addOrder = document.querySelector(".addOrder")
+// const pizzaSize = document.querySelector(".pizzaSize")
+// const pizzaToppings = document.querySelector(".pizzaToppings")
+// // const addOrder = document.querySelector(".addOrder")
 
-
+// UI Logic
 window.addEventListener("load", function() {
     console.log("here")
-    document.querySelector("form").addEventListener("submit", PizzaOrder)
+    document.querySelector("form").addEventListener("submit", handleFormSubmission)
 });
 
 function handleFormSubmission(event){
     event.preventDefault()
-    const usersInput = document.getElementsByClassName("pizzaOrder").value
-    const outPut = array(usersInput)
+    const sizeValue = document.querySelector('input[name="pizzaSize"]:checked').value
+    const toppings = document.querySelector('input[name="toppings"]:checked').value
+    console.log(toppings)
+    // const outPut = array(sizeValue)
+    let order = new PizzaOrder() -- can run protypes on, going through and adding values of size of toppings  -- parseInt to change user inputs from strings to / 
     document.getElementById("pizzaTotal").innerText = outPut
 };
+
+// make sure to follow TDD objects for both
